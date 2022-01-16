d0c48e702611715a905b24b52bf521e168d8a70c
BenHerbst <ben.herbst@web.de>
2022-01-16 19:51:14 +0100

    Basic commands

--- openasis/core/core.py
+++ openasis/core/core.py
@@ -1,4 +1,22 @@
+import re
+import webbrowser
+
 def take_query(raw_command):
     command = raw_command.lower().split()
-    if command[0] == "hello":
-        return "hi, how are you?"
+    if command[0] == 'open':
+        if command[1] == 'duckduckgo':
+            webbrowser.open("https://duckduckgo.com/")
+            return 'Opening duckduckgo.com'
+        elif command[1] == 'google':
+            webbrowser.open('https://google.com/')
+            return 'Opening google.com'
+    elif command[0] == 'search':
+        if 'with duckduckgo' in raw_command.lower():
+            webbrowser.open('https://duckduckgo.com/?q=' + replace_ignore_case( replace_ignore_case(raw_command, 'search',  ''), 'with duckduckgo', ''))
+            return 'Searching...'
+    else:
+        return "I can't understand you."
+
+def replace_ignore_case(text, what_to_replace, replace_with):
+    compiled = re.compile(re.escape(what_to_replace), re.IGNORECASE)
+    return compiled.sub(replace_with, text)
--- openasis/desktop/desktop.py
+++ openasis/desktop/desktop.py
@@ -1,6 +1,7 @@
 from PySide6 import QtCore, QtGui, QtWidgets
 from PySide6.QtWidgets import QWidget
 import openasis.core.core as core
+import pyttsx3
 
 class Window(QWidget):
     def __init__(self):
@@ -17,7 +18,13 @@ class Window(QWidget):
             widget is self.edit):
             key = event.key()
             if key == QtCore.Qt.Key_Enter or key == QtCore.Qt.Key_Return:
-                self.text_area.append(core.take_query(self.edit.text()))
+                answer = core.take_query(self.edit.text())
+                self.text_area.append(answer)
+                self.edit.setText("")
+                tts_engine = pyttsx3.init()
+                tts_engine.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0')
+                tts_engine.say(answer)
+                tts_engine.runAndWait()
 
 def main():
     import sys
@@ -26,3 +33,11 @@ def main():
     window.setGeometry(500, 300, 300, 300)
     window.show()
     sys.exit(app.exec_())
+
+def change_voice(engine, language, gender='VoiceGenderFemale'):
+    for voice in engine.getProperty('voices'):
+        if language in voice.languages and gender == voice.gender:
+            engine.setProperty('voice', voice.id)
+            return True
+
+    raise RuntimeError("Language '{}' for gender '{}' not found".format(language, gender))
